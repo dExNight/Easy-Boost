18773fb48ee1af2560a048e5a1b5ec18155a9214
dExNight <am.gubaidullin@innopolis.university>
2024-11-04 16:48:51 +0300

    feat: contract wrappers

---
+++ app/src/contracts/Boost.ts
@@ -0,0 +1,110 @@
+import {
+  Address,
+  beginCell,
+  Cell,
+  Contract,
+  contractAddress,
+  ContractProvider,
+  Sender,
+  SendMode,
+  TupleBuilder,
+} from "@ton/core";
+import { Opcodes, Gas } from "./constants";
+
+export type BoostConfig = {
+  poolAddress: Address;
+  boostIndex: number;
+  nftItemCode: Cell;
+  boostHelperCode: Cell;
+};
+
+export function boostConfigToCell(config: BoostConfig): Cell {
+  return beginCell()
+    .storeUint(0, 1)
+    .storeAddress(config.poolAddress)
+    .storeUint(config.boostIndex, 32)
+    .storeRef(config.nftItemCode)
+    .storeRef(config.boostHelperCode)
+    .endCell();
+}
+
+export class Boost implements Contract {
+  constructor(
+    readonly address: Address,
+    readonly init?: { code: Cell; data: Cell }
+  ) {}
+
+  static createFromAddress(address: Address) {
+    return new Boost(address);
+  }
+
+  static createFromConfig(config: BoostConfig, code: Cell, workchain = 0) {
+    const data = boostConfigToCell(config);
+    const init = { code, data };
+    return new Boost(contractAddress(workchain, init), init);
+  }
+
+  async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+    await provider.internal(via, {
+      value,
+      sendMode: SendMode.PAY_GAS_SEPARATELY,
+      body: beginCell().endCell(),
+    });
+  }
+
+  async sendSetBoostJettonWallet(
+    provider: ContractProvider,
+    via: Sender,
+    walletAddress: Address,
+    query_id: number = 0
+  ) {
+    await provider.internal(via, {
+      value: Gas.set_boost_wallet_address,
+      sendMode: SendMode.PAY_GAS_SEPARATELY,
+      body: beginCell()
+        .storeUint(Opcodes.set_boost_wallet_address, 32)
+        .storeUint(query_id, 64)
+        .storeAddress(walletAddress)
+        .endCell(),
+    });
+  }
+
+  async getBoostData(provider: ContractProvider) {
+    const result = (await provider.get("get_boost_data", [])).stack;
+    return {
+      init: result.readNumber(),
+      poolAddress: result.readAddress(),
+      boostIndex: result.readNumber(),
+      creatorAddress: result.readAddress(),
+      startTime: result.readNumber(),
+      endTime: result.readNumber(),
+      snapshotItemIndex: result.readNumber(),
+      snapshotTvl: result.readBigNumber(),
+      totalRewards: result.readBigNumber(),
+      farmingSpeed: result.readBigNumber(),
+      boostWalletAddress: result.readAddressOpt(),
+    };
+  }
+
+  async getBoostCode(provider: ContractProvider) {
+    const result = (await provider.get("get_boost_codes", [])).stack;
+
+    return {
+      nftItemCode: result.readCell(),
+      boostHelperCode: result.readCell(),
+    };
+  }
+
+  async getBoostHelperAddress(
+    provider: ContractProvider,
+    nftItemAddress: Address
+  ) {
+    const params = new TupleBuilder();
+    params.writeAddress(nftItemAddress);
+    const result = (
+      await provider.get("get_boost_helper_address", params.build())
+    ).stack;
+
+    return result.readAddress();
+  }
+}
---
+++ app/src/contracts/NftItem.ts
@@ -0,0 +1,178 @@
+import {
+  Address,
+  beginCell,
+  Cell,
+  Contract,
+  contractAddress,
+  ContractProvider,
+  Sender,
+  SendMode,
+  toNano,
+} from "@ton/core";
+import { Gas, Opcodes } from "./constants";
+
+export type NftItemConfig = {};
+
+export function nftItemConfigToCell(config: NftItemConfig): Cell {
+  return beginCell().endCell();
+}
+
+export class NftItem implements Contract {
+  constructor(
+    readonly address: Address,
+    readonly init?: { code: Cell; data: Cell }
+  ) {}
+
+  static createFromAddress(address: Address) {
+    return new NftItem(address);
+  }
+
+  static createFromConfig(config: NftItemConfig, code: Cell, workchain = 0) {
+    const data = nftItemConfigToCell(config);
+    const init = { code, data };
+    return new NftItem(contractAddress(workchain, init), init);
+  }
+
+  async sendTransferNft(
+    provider: ContractProvider,
+    via: Sender,
+    opts: {
+      toAddress: Address;
+      forwardAmount: bigint;
+      forwardPayload?: Cell;
+      queryId?: number;
+    }
+  ) {
+    await provider.internal(via, {
+      value: Gas.jetton_transfer,
+      sendMode: SendMode.PAY_GAS_SEPARATELY,
+      body: beginCell()
+        .storeUint(Opcodes.transfer_nft, 32)
+        .storeUint(opts.queryId ?? 0, 64)
+        .storeAddress(opts.toAddress)
+        .storeAddress(via.address)
+        .storeBit(0)
+        .storeCoins(opts.forwardAmount)
+        .storeMaybeRef(opts.forwardPayload)
+        .endCell(),
+    });
+  }
+
+  async sendClaimNft(
+    provider: ContractProvider,
+    via: Sender,
+    opts: {
+      queryId?: number;
+    }
+  ) {
+    await provider.internal(via, {
+      value: Gas.claim_nft,
+      sendMode: SendMode.PAY_GAS_SEPARATELY,
+      body: beginCell()
+        .storeUint(Opcodes.claim_nft, 32)
+        .storeUint(opts.queryId ?? 0, 64)
+        .endCell(),
+    });
+  }
+
+  async sendClaimBoostRewards(
+    provider: ContractProvider,
+    via: Sender,
+    boostAddress: Address,
+    queryId?: number
+  ) {
+    await provider.internal(via, {
+      value: Gas.claim_boost_rewards,
+      sendMode: SendMode.PAY_GAS_SEPARATELY,
+      body: beginCell()
+        .storeUint(Opcodes.claim_boost_rewards, 32)
+        .storeUint(queryId ?? 0, 64)
+        .storeAddress(boostAddress)
+        .endCell(),
+    });
+  }
+
+  async sendWithdrawNft(
+    provider: ContractProvider,
+    via: Sender,
+    opts: {
+      queryId?: number;
+    }
+  ) {
+    await provider.internal(via, {
+      value: Gas.withdraw_nft,
+      sendMode: SendMode.PAY_GAS_SEPARATELY,
+      body: beginCell()
+        .storeUint(Opcodes.withdraw_nft, 32)
+        .storeUint(opts.queryId ?? 0, 64)
+        .endCell(),
+    });
+  }
+
+  async sendWithdrawJetton(
+    provider: ContractProvider,
+    via: Sender,
+    opts: {
+      jettonWallet: Address;
+      jettonAmount: number;
+      queryId?: number;
+    }
+  ) {
+    await provider.internal(via, {
+      value: Gas.jetton_transfer,
+      sendMode: SendMode.PAY_GAS_SEPARATELY,
+      body: beginCell()
+        .storeUint(3, 32)
+        .storeUint(opts.queryId ?? 0, 64)
+        .storeAddress(opts.jettonWallet)
+        .storeCoins(opts.jettonAmount)
+        .endCell(),
+    });
+  }
+
+  async sendWithdrawTon(
+    provider: ContractProvider,
+    via: Sender,
+    opts: {
+      queryId?: number;
+    }
+  ) {
+    await provider.internal(via, {
+      value: toNano("0.01"),
+      sendMode: SendMode.PAY_GAS_SEPARATELY,
+      body: beginCell()
+        .storeUint(4, 32)
+        .storeUint(opts.queryId ?? 0, 64)
+        .endCell(),
+    });
+  }
+
+  async getNftData(provider: ContractProvider) {
+    const result = (await provider.get("get_nft_data", [])).stack;
+    return {
+      init: result.readNumber(),
+      index: result.readNumber(),
+      collection_address: result.readAddress(),
+      owner_address: result.readAddress(),
+      content: result.readCell(),
+    };
+  }
+
+  async getStorageData(provider: ContractProvider) {
+    const result = (await provider.get("get_storage_data", [])).stack;
+    return {
+      init: result.readNumber(),
+      index: result.readNumber(),
+      collectionAddress: result.readAddress(),
+      ownerAddress: result.readAddress(),
+      lockedValue: result.readNumber(),
+      startTime: result.readNumber(),
+      unlockTime: result.readNumber(),
+      withdrawedAt: result.readNumber(),
+      claimedRewards: result.readBigNumber(),
+      isTransferrable: result.readNumber(),
+      isActive: result.readNumber(),
+      distributedRewards: result.readBigNumber(),
+    };
+  }
+}
